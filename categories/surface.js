0dc1848ccba7b2bc9e66010dc3a99c19697a2252
TheKlem <80773810+TheKlem@users.noreply.github.com>
2024-04-26 09:44:43 +0200

    maj surfaces

--- categories/surface.js
+++ categories/surface.js
@@ -16,7 +16,7 @@ var conversions = [
     
 ]
 
-AppConversions.AddGroup(category.value, "Unités standard", "", conversions);
+AppConversions.AddGroup(category.value, "Unités standard", "font-weight: bold; color: red;", conversions);
 
 var conversions = [
     ["Banane","0.003", "font-weight: bold; color: limegreen;"],
@@ -34,7 +34,7 @@ var conversions = [
 ["Logement moyen en France (locataire)","68", ""],
 ]
 
-AppConversions.AddGroup(category.value, "Objets et divers", "", conversions);
+AppConversions.AddGroup(category.value, "Objets et divers", "font-weight: bold; color: red;", conversions);
 
 var conversions = [
     ["Table de ping-pong","4.183", ""],
@@ -49,14 +49,13 @@ var conversions = [
 
 ]
 
-AppConversions.AddGroup(category.value, "Sports", "", conversions);
+AppConversions.AddGroup(category.value, "Sports", "font-weight: bold; color: red;", conversions);
 
 var conversions = [
     ["Eolienne, emrpise au sol (2MW)","2000", ""],
 ["Eolienne, emprise physique (2MW)","20000", ""],
 ["Exploitation agricole moyenne en France","690000", ""],
 ["Exploitation agricole moyenne DOM-TOM","50000", ""],
-["Exploitation viticole moyenne en Champagne","54000", ""],
 ["Exploitation viticole moyenne en Champagne","54000", ""],
 ["Exploitation viticole moyenne en Bourgogne","85000", ""],
 ["Exploitation viticole moyenne en Bordelais","170000", ""],
@@ -67,38 +66,59 @@ var conversions = [
 
 ]
 
-AppConversions.AddGroup(category.value, "Agriculture", "", conversions);
+AppConversions.AddGroup(category.value, "Agriculture", "font-weight: bold; color: red;", conversions);
 
 var conversions = [
-    ["Place du Trocadéro","21000", ""],
-["Cimetière du Pêre Lachaise","439300", ""],
-["Ensemble des cimetières parisiens","926600", ""],
-["Cîté des morts, Le Caire","7000000", ""],
-["Ikea Grand Parilly","40000", ""],
-["Champs de mars","245000", ""],
-["Bois de Boulogne","8460000", ""],
-["Bois de Vincennes","9950000", ""],
-["Parc Monceau (Paris)","80000", ""],
-["Parc de la Tête d'or (Lyon)","1050000", ""],
-["Central Park","3410000", ""],
-["Vatican","440000", ""],
-["Las Vegas","352000000", ""],
+    ["Ikea Grand Parilly","40000", ""],
+["Le Bon Marché, Paris","10000", ""],
+["Galerie Lafayette, Paris","70000", ""],
 ["Parc des expositions de Villepinte","46000", ""],
+["Casino Mandala Bay, Las Vegas, surface jeux","12541", ""],
 ["Parc Asterix","340000", ""],
 ["Disneyland Paris","22300000", ""],
+["Le Louvre, surface au sol","360000", ""],
+["Le Louvre, surface de plancher","243000", ""],
+["Le Louvre, surface visitable","60000", ""],
+["Chateau de Versaille, surface de plancher","63154", ""],
+["Chateau de Versaille, surface totale","8150000", ""],
 ["Notre-Dame de Paris","6000", ""],
 ["Notre-Dame d'Amiens","7700", ""],
 ["Pyramide de Khéops","52900", ""],
-["Forêt de Fontainebleau","177020000", ""],
 ["Aéroport Roissy Charles de Gaulle","32380000", ""],
+
+]
+
+AppConversions.AddGroup(category.value, "Monuments et bâtiments", "font-weight: bold; color: red;", conversions);
+
+var conversions = [
+    ["Place du Trocadéro","21000", ""], 
+    ["Place de l'Etoile","45500", ""], 
+    ["Place de la Concorde","86400", ""],
+    ["Place Bellecour, Lyon","62000", ""],
+    ["Place Rouge, Moscou","23100", ""],
+    ["Place du Tien An Men, Pekin","400000", ""],
+    ["Cimetière du Pêre Lachaise","439300", ""],
+    ["Ensemble des cimetières parisiens","926600", ""],
+    ["Cîté des morts, Le Caire","7000000", ""],
+    ["Champs de mars","245000", ""],
+    ["Bois de Boulogne","8460000", ""],
+    ["Bois de Vincennes","9950000", ""],
+    ["Forêt de Fontainebleau","177020000", ""],
+    ["Parc Monceau (Paris)","80000", ""],
+    ["Parc de la Tête d'or (Lyon)","1050000", ""],
+    ["Central Park","3410000", ""],
+["Vatican","440000", ""],
+["Las Vegas","352000000", ""],
 ["Paris","105000000", ""],
 ["Lyon","48870000", ""],
 ["Marseille","240600000", ""],
+["Normandie","30627000000", ""],
 ["France","550000000000", ""],
+["Suisse","41285000000", ""],
 ["Superficie terrestre : terres émergées","148000000000000", ""],
 ["Superficie terrestre : oceans et mers","362000000000000", ""],
 ["Superficie  terrestre","510000000000000", ""],
 
 ]
 
-AppConversions.AddGroup(category.value, "Monuments et géographie", "", conversions);
+AppConversions.AddGroup(category.value, "Géographie", "font-weight: bold; color: red;", conversions);
